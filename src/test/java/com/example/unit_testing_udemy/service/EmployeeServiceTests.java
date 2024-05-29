3b95996d5326ee78d2941837c113aca39a734057
waelhasnaoui1 <waelhasnaoui006@gmail.com>
2024-05-29 16:46:49 +0100

    mocking the methods of the repository

--- src/test/java/com/example/unit_testing_udemy/service/EmployeeServiceTests.java
+++ src/test/java/com/example/unit_testing_udemy/service/EmployeeServiceTests.java
@@ -5,37 +5,51 @@ import com.example.unit_testing_udemy.repository.EmployeeRepository;
 import com.example.unit_testing_udemy.service.impl.EmployeeServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class EmployeeServiceTests {
 
-
+    /// using @Mock to to mock the needed object
+    @Mock
     private EmployeeRepository employeeRepository;
 
-
-    private EmployeeService employeeService;
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
 
     private Employee employee;
 
     @BeforeEach
     public void setup(){
         employee = Employee.builder().firstName("wael").lastName("has").email("wael@gmail.com").build();
-        employeeRepository = Mockito.mock(EmployeeRepository.class);
-        employeeService = new EmployeeServiceImpl(employeeRepository);
+        MockitoAnnotations.openMocks(this);
+
+        /// mocking using Mockito.mock
+//        employeeRepository = Mockito.mock(EmployeeRepository.class);
+//        employeeService = new EmployeeServiceImpl(employeeRepository);
     }
 
     @Test
+    @DisplayName("testing were are saving an employee")
     public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployee(){
         // given
-
+        /// we add this because mployeeService.saveEmployee call the method employeeRepository.save() so we need to mock it 
+        BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
 
         // when
         Employee savedEmployee = employeeService.saveEmployee(employee);
         System.out.println("the saved employee is"+savedEmployee);
         // then
+
+//        Assertions.assertThat(savedEmployee).isNull();
+        ///// herer were verifying that the method employeeRepository.save() is called once
+        verify(employeeRepository, times(1)).save(employee);
+
+        /// here we want to verify that employeeService.saveEmployee() will not return an empty object
 
         Assertions.assertThat(savedEmployee).isNotNull();
     }
